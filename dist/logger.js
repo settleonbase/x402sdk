04c95f5a35796f41aa0fffc44686d585c8b22bd5
CoNET Project <info@CoNETTech.ca>
2025-10-31 11:58:26 -0700

    Stop tracking dist folder

--- dist/logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const logger = (...argv) => {
-    const date = new Date();
-    const dateStrang = `%c [worker INFO ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}]`;
-    return console.log(dateStrang, 'color: #6f4de7ff', ...argv);
-};
-exports.logger = logger;
