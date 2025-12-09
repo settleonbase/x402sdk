eca4fd26b80cd5a9b974be4126337c1178a71d63
CoNET Project <info@CoNETTech.ca>
2025-12-08 23:47:19 -0800

    update logger for cluster

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,18 @@
+import cluster from 'cluster'
 
-export  const logger = (...argv: any ) => {
-    const date = new Date ()
-    const dateStrang = `%c [worker INFO ${ date.getHours() }:${ date.getMinutes() }:${ date.getSeconds() }:${ date.getMilliseconds ()}]`
-    return console.log ( dateStrang, 'color: #6f4de7ff',  ...argv)
+export const logger = (...argv: any[]) => {
+  const date = new Date()
+
+  // 格式化时间
+  const ts = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}`
+
+  // cluster 信息
+  const wid = cluster.isPrimary
+    ? 'master'
+    : `worker-${cluster.worker?.id ?? 'unknown'}`
+
+  // 彩色 prefix
+  const prefix = `%c[${wid} INFO ${ts}]`
+
+  return console.log(prefix, 'color: #6f4de7ff', ...argv)
 }
-
