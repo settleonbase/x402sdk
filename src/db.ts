0165fcba186a5b144689c3f1f4a31c2100dea9fe
CoNET Project <info@CoNETTech.ca>
2025-12-11 20:05:26 -0800

    add follow bug fix

--- src/db.ts
+++ src/db.ts
@@ -723,7 +723,7 @@ export const removeFollow = ( req: Request, res: Response) => {
 	})
 	res.status(200).json({ok: true}).end()
 	addFollowPoolProcess()
-	updateUserFollowRemoveDB(wallet, followAddress)	
+	
 }
 
 
