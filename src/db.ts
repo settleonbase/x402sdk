49399408970dbcc39ea4eab9238681ca52353f15
CoNET Project <info@CoNETTech.ca>
2025-12-21 21:30:42 -0800

    add admin account

--- src/db.ts
+++ src/db.ts
@@ -1324,7 +1324,7 @@ export type FollowUserItem = {
 
 // test()
 
-const admin = ['0x678F3570F9173373bB75e7544fcF383153aDAF4C','0x87cAeD4e51C36a2C2ece3Aaf4ddaC9693d2405E1','0x24103Ea5eA17aBFfDA8b2904acCA679C798b3695', '0x66BAb8A64764e659Fa7FF41D19aDFbb7b956CED2']
+const admin = ['0x66BAb8A64764e659Fa7FF41D19aDFbb7b956CED2']
 const img = `https://beamio.app/favicon.ico`
 
 const addUserAdmin = async () => {
