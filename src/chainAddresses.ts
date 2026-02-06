e90e351668b0f406f1b2d31e39ca70d8aac4496e
CoNET Project <info@CoNETTech.ca>
2026-02-06 00:24:49 -0800

    update

--- src/chainAddresses.ts
+++ src/chainAddresses.ts
@@ -2,4 +2,4 @@
  * Base 主网 AA Factory 地址。
  * 与 config/base-addresses.ts 保持一致；运行 npm run redeploy:aa-factory:base 后会同步更新此处。
  */
-export const BASE_AA_FACTORY = '0xB8AB5fdD3f5097b2A0887fe9Ab2B605b0a546BD1'
+export const BASE_AA_FACTORY = '0x4811fe90Bb3b3C5dE6491c5Efb90E19756F2C318'
